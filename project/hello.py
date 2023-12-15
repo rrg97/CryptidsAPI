648dbc8061ce3f1b5c5d0cfb3881646b38043f16
Ruben Giraldez <ruben@Rubens-MacBook-Air.local>
2023-12-15 21:09:49 +0100

    Added project structure

--- project/hello.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/hi')
-def greet(who, status_code=200):
-    return f"Hello? {who}?"
---
+++ src/fake/explorer.py
@@ -0,0 +1,35 @@
+from model.explorer import Explorer
+
+_explorers = [
+    Explorer(name="Claude Hande",
+             country="FR",
+             description="Scarce during full moons"),
+    Explorer(name="Noah Weiser",
+             country="DE",
+             description="Myopic machete man"),
+    ]
+
+def get_all() -> list[Explorer]:
+    return _explorers
+
+def get_one(name: str) -> Explorer | None:
+    for _explorer in _explorers:
+        if _explorer.name == name:
+            return _explorer
+    return None
+
+def create(explorer: Explorer) -> Explorer:
+    """Add an explorer"""
+    return explorer
+
+def modify(explorer: Explorer) -> Explorer:
+    """Partially modify an explorer"""
+    return explorer
+
+def replace(explorer: Explorer) -> Explorer:
+    """Completely replace an explorer"""
+    return explorer
+
+def delete(name: str) -> bool:
+    """Delete an explorer; return None if it existed"""
+    return None
